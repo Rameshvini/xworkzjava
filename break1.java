4c56bc2cf8337c75cc16d3d554dcdf8553092f50
Rameshvini <147069055+Rameshvini@users.noreply.github.com>
2023-12-27 14:55:44 +0530

    1

---
+++ break1.java
@@ -0,0 +1,16 @@
+package controlconstruct;
+
+public class break1 
+{
+public static void main(String[] args) 
+{
+	for(int i=0;i<=5;i++)
+	{
+		System.out.println("kiran");
+		if(i==2)
+		{
+			break;
+		}
+	}
+}
+}
