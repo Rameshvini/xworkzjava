b34984e53c8636e61f79dcf5a9832b7ff38210c8
Rameshvini <147069055+Rameshvini@users.noreply.github.com>
2023-12-27 15:07:43 +0530

    sql

---
+++ First.sql
@@ -0,0 +1,11 @@
+create database college;
+use college;
+create table studentinfo
+(
+id int,
+name varchar(5),
+roll int
+);
+insert into studentinfo values(12,'ram',23);
+select * from  studentinfo;
+show tables;
